085f7b6e1dc5a6678e1c478b21c6449f20f3afb4
Kouler <beeproger@yandex.ru>
2026-01-15 21:49:20 +1000

    vault backup: 2026-01-15 21:49:20

---
+++ Код/Задание 2/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {    
+    for (int w = 0; w < 2; w++) {
+        for (int x = 0; x < 2; x++) {
+            for (int y = 0; y < 2; y++) {
+                for (int z = 0; z < 2; z++) {
+                    if (
+                        ((x == (w || y)) || ((w <= z) && (y <= w))) == 0
+                    )
+                        std::cout << w << ' ' << x << ' ' << y << ' ' << z << '\n';
+                }
+            }
+        }
+    }
+}
