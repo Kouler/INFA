4741bd7ac9a8f19ff803135d81e0d3fe45a0045e
Kouler <beeproger@yandex.ru>
2026-03-03 11:35:55 +1000

    vault backup: 2026-03-03 11:35:55

--- Код/16.cpp
+++ Код/16.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 
 int F(int n) {
-    if (n == 0) {
-        return 0;
-    }
-
-    return F(n / 10) + (n % 10);
+    return n ? F(n / 10) + (n % 10) : 0;
 }
 
 int main() {
     int count = 0;
 
+    std::cout << F(554);
+
     int percent = (1134567010 - 237567892) / 100;
     int out = 0;
+    /*
     for (int i = 237567892; i < 1134567010; i++)  {
         if (i % percent == 0) {
             std::cout << out << "%\n";
@@ -23,6 +22,7 @@ int main() {
     }
 
     std::cout << "Res: " << count << '\n';
+    */
     
 
 
